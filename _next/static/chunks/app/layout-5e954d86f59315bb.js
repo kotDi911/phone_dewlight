2fb917e827b88336741900cf7e4db6e97d70fe7f
= <=>
2025-10-23 13:17:45 +0300

    Updates

---
+++ _next/static/chunks/app/layout-5e954d86f59315bb.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[177],{3569:()=>{},9552:(e,s,_)=>{Promise.resolve().then(_.t.bind(_,3569,23))}},e=>{e.O(0,[136,441,255,358],()=>e(e.s=9552)),_N_E=e.O()}]);
